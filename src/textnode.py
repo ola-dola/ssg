4f63c789a7202415023ab8981cf1d00e118310f5
Ola Oredola <oredolaolamide@gmail.com>
2025-05-22 09:02:14 +0100

    inline markdown to html

--- src/textnode.py
+++ src/textnode.py
@@ -31,24 +31,27 @@ class TextNode:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
 
-def text_node_to_html_node(text_node: TextNode):
-    if text_node.text_type not in TextType:
-        raise ValueError("Invalid text node type")
+def text_node_to_html_node(text_node: TextNode): 
+    html_node = None
 
     match text_node.text_type:
         case TextType.TEXT:
-            LeafNode(tag=None, value=text_node.text)
+            html_node = LeafNode(tag=None, value=text_node.text)
         case TextType.BOLD:
-            LeafNode(tag="b", value=text_node.text)
+            html_node = LeafNode(tag="b", value=text_node.text)
         case TextType.ITALIC:
-            LeafNode(tag="i", value=text_node.text)
+            html_node = LeafNode(tag="i", value=text_node.text)
         case TextType.CODE:
-            LeafNode(tag="code", value=text_node.text)
+            html_node = LeafNode(tag="code", value=text_node.text)
         case TextType.LINK:
-            LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+            html_node = LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
         case TextType.IMAGE:
-            LeafNode(
+            html_node = LeafNode(
                 tag="img",
-                value=None,
+                value="",
                 props={"src": text_node.url, "alt": text_node.text},
             )
+        case _:
+            raise ValueError("Invalid text node type")
+        
+    return html_node
